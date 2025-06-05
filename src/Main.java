94ced50d63ba4b5a33750f4857bbc888cd5f7f78
gno <gno0877@naver.com>
2025-06-05 14:14:33 +0900

    3 commit functions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
+    public static void printStudent(String name, int age, String sex) {
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("성별: " + sex);
+        System.out.println("------");
+
+    }
     public static void main(String[] args) {
-        int score = 61;
-
-        boolean result = (score > 60) ? true : false;
-
-        System.out.println(result);
+        System.out.println("=== 학생 정보 ===");
+        Main.printStudent("alice", 20,"woman");
+        printStudent("bob", 22, "man");
+        printStudent("charlie", 25,"man");
     }
 }
