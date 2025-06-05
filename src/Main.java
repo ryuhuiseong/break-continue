bdf73bfcae65b2ce254fe66b74e1161acce2bf4c
gno <gno0877@naver.com>
2025-06-05 11:22:27 +0900

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i == 7) {
-                break;
-            }
+        int score = 60;
 
-            System.out.println(i);
+        String result;
+
+        if (score > 59) {
+            result = "합격";
+        }else {
+            result = "불합격";
         }
+        System.out.println("결과 : " + result);
     }
 }
