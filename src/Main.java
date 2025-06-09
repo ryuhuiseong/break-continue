d4ea0961c3b29c441641aa6ee3aaca0d97b9d1b2
gno <gno0877@naver.com>
2025-06-09 10:37:24 +0900

    3 commit 메서드오버로딩

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
-    public static int add(int a, int b){
-        return a + b;
+    public static void greet(){
+        System.out.println("Hello, World!");
     }
 
-    public static  int add(int a, int b, int c){
-        return  a + b + c;
+    public static void greet(String name){
+        System.out.println("Hello, " + name + "!");
     }
 
-
-    public static void main(String[] args) {
-        System.out.println("a + b = " + add(1, 2));
-        System.out.println("a + b + c = " +add(3, 4, 5));
-
-        System.out.println(1);
+    public static void main(String[] args){
+        greet();
+        greet("왕운찬");
     }
-}
+    }
