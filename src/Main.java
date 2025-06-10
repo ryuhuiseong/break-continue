68784d5c4029454babae37775a02bc57a78eafd8
gno <gno0877@naver.com>
2025-06-10 10:24:10 +0900

    1 commit class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
+
+class Person {
+    String name;
+    int age;
+    String MBTI;
+    String work;
+
+    public void sayHello(){
+        System.out.println("안녕하세요? 저는 " + name + "입니다. 저의 나이는 " + age + "세 입니다." + "MBTI는 " + MBTI + "입니다.");
+    }
+
+    public void work() {
+        System.out.println("일하는 중...");
+    }
+ }
 public class Main {
-    public static void greet(){
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] agrs ) {
+        Person person = new Person();
 
-    public static void greet(String name){
-        System.out.println("Hello, " + name + "!");
-    }
+        person.name = "류희성";
+        person.age = 26;
+        person.MBTI = "ENFP";
 
-    public static void main(String[] args){
-        greet();
-        greet("왕운찬");
+        person.sayHello();
+        person.work();
     }
-    }
+}
